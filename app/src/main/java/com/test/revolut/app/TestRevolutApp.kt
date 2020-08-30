7d7eb60a1ecadb20841d169e74b841e7a270ceac
masteroffreedom <masteroffreedom@ya.ru>
2020-08-31 00:33:13 +0300

    init timber

--- app/src/main/java/com/test/revolut/app/TestRevolutApp.kt
+++ app/src/main/java/com/test/revolut/app/TestRevolutApp.kt
@@ -1,7 +1,18 @@
 package com.test.revolut.app
 
 import android.app.Application
+import com.test.revolut.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 @HiltAndroidApp
-class TestRevolutApp : Application()
+class TestRevolutApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
